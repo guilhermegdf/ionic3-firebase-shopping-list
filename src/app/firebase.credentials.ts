2ec2d1cd886573cdc2cd8344c0d6be0f8d7e4f52
jdesfon <jdesfon@gmail.com>
2018-08-06 18:00:05 +0200

    first commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAD92-8PmttSW_7yvfz9VudB3vyzacHSws",
+    authDomain: "ionicshoppinglist-b3740.firebaseapp.com",
+    databaseURL: "https://ionicshoppinglist-b3740.firebaseio.com",
+    projectId: "ionicshoppinglist-b3740",
+    storageBucket: "ionicshoppinglist-b3740.appspot.com",
+    messagingSenderId: "838406707266"
+};
